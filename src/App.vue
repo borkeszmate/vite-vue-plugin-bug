9f8fedbc3f3c718ea3ba3f46ba05e2b33a3cf9f5
Máté Borkesz <mate.borkesz@feat.agency>
2021-10-21 19:40:07 +0200

    Faulty Vite project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="bg-white shadow" v-if="$route.meta.title">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1
+          @click="counter = 0"
+          class="text-3xl font-bold leading-tight text-gray-900"
+        >
+        </h1>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
